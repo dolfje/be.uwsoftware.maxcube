72870e1c58a54d3ae4aa7b4c172a6c8da5b2c8d4
Nikos Verschore <nikos@uwsoftware.be>
2019-01-04 11:56:20 +0100

    Test2

--- drivers/max_door/driver.js
+++ drivers/max_door/driver.js
@@ -6,7 +6,9 @@ const AbstractDriver = require('../abstract_driver');
 class MaxPlusDriver extends AbstractDriver {
 
     toHomeyDevice(cube, device, deviceInfo) {
+    	console.log("Device asked "+device.device_type);
         if(device.device_type == 6) {
+        	console.log("Found device");
             return super.toHomeyDevice(cube, device, deviceInfo);
         }
         return false;
